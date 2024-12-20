ba1a95d426bae8a48740108d8e137c1f6883d0f3
Sindhu Baskar <153032345+sindhujabaskar@users.noreply.github.com>
2024-12-19 19:19:35 -0500

    added load function, created simulated vis stim, fixed df/f mistake
    starting to clean up and parameterize the code by adding functions. created a simulated vis stim to play with event related epochs in the fluorescence. dff is still looking strange

--- s2p-dff-plotter.py
+++ s2p-dff-plotter.py
@@ -1,93 +1,96 @@
-#%% GET LIBRARIES
+# %% GET LIBRARIES
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
-#%% SET DIRECTORY PATH
-pathlist = Path(r'/Volumes/Sinbas_Stuf/Projects/SU24_F31/raw/sub-03/baseline-SB03/suite2p/plane0').glob('*.npy')
+# %% DEFINE FUNCTIONS
 
-#%% LOAD ALL SUITE2P OUTPUTS WITHIN DIRECTORY
+def load_suite2p_outputs(directory_path):
+    file_dict = {
+        'roi_fluorescence': '*F.npy',
+        'neuropil_fluorescence': '*Fneu.npy',
+        'cell_identifier' : '*iscell.npy',
+        'intermediate_outputs' : '*ops.npy',
+        'roi_traces' : '*spks.npy',
+        'roi_statistics' : '*stat.npy'
+    }
+    loaded_data_files = {}
+    pathlist = Path(directory_path)
 
-file_list = [] # initiate empty list
-for path in pathlist:  
-    print(f'this is the current path {path}')
-    file_path = str(path)
-    print(f'this is the file_path found {file_path}')
-    file_list.append(file_path)
-    print(f'this is the appending list {file_list}')
+    for key, value in file_dict.items():
+        suite2p_files = list(pathlist.glob(value))
+        for file in suite2p_files:
+            loaded_data_files[key] = np.load(file, allow_pickle = True)
+    return loaded_data_files
 
-
-print(file_list)
-
-for file in file_list:
-    roi_fluorescence = np.load(file_list[0], allow_pickle = True) # ALL ROI FLUORESCENCE
-    neuropil_fluorescence = np.load(file_list[1], allow_pickle = True) # NEUROPIL FLUORESCENCE 
-    cell_identifier = np.load(file_list[2], allow_pickle = True) # INDEX FOR DETERMINING ROI AS CELL
-    intermediate_outputs = np.load(file_list[3], allow_pickle = True) # SUITE2P RUN SESSION METADATA
-    roi_traces = np.load(file_list[4], allow_pickle = True) # DECONVOLVED TRACES
-    roi_statistics = np.load(file_list[5], allow_pickle = True) #STATISTICS PER ROI
-
-print(roi_fluorescence)
-
-#%% FILTER ROIs (CELLS ONLY)
+# %% LOAD DATA
+suite2p_data_output = load_suite2p_outputs(r'/Volumes/Sinbas_Stuf/Projects/SU24_F31/raw/sub-03/baseline-SB03/suite2p/plane0')
+    
+# %% FILTER ROIs (CELLS ONLY)
 
 # assign bool T/F based on confidence index
-true_cells_only = cell_identifier[:,0].astype(bool)
+true_cells_only = suite2p_data_output['cell_identifier'][:,0].astype(bool)
 print(true_cells_only)
 
 # filter ROIs and neuropil based on bool value
-filtered_roi = np.array(roi_fluorescence[true_cells_only])
-filtered_neuropil = np.array(neuropil_fluorescence[true_cells_only])
+filtered_roi = np.array(suite2p_data_output['roi_fluorescence'][true_cells_only])
+filtered_neuropil = np.array(suite2p_data_output['neuropil_fluorescence'][true_cells_only])
 
-#
-num_roi = len(filtered_roi)
-
-#%% NEUROPIL SUBTRACTION
-
+# %% NEUROPIL SUBTRACTION
 neuropil_subtracted_roi = (filtered_roi - filtered_neuropil)
 
 #%% PLOT ROIS
-
-import matplotlib.pyplot as plt
-
-roi_number = neuropil_subtracted_roi[3,:]
-avg_fluorescence = np.mean
-
-plt.plot(range(len(roi_number)), roi_number)
+plt.plot(neuropil_subtracted_roi[2])
+plt.title('Raw Fluorescence of ROI')
+plt.xlabel('frames')
+plt.ylabel('raw fluorescence')
 plt.show()
 
-#%% CALCULATE DF/F
-
-# i have an ndarray containing the fluorescence values of the neuropil subtracted rois
-# i want to populate another array or a list with fluorescence values emcompassing a baseline percentile
-# i want to then use the percentile array as my "baseline fluorescence" aka F0
-# then i must perform a (f-f0)/(f0) calculation for each ROI, at each frame to get a dff output 
-# I want to then plot an overlay of before and after percentile normalization of ROI fluorescence 
-
+# %% CALCULATE BASELINE FLUORESCENCE
 # initiate empty list
 percentile_list = []
-fluorescence_percentile = 5 # set percentile value for calculating baseline fluorescence (f0)
-for row in neuropil_subtracted_roi[:,:]:
-    percentile = np.percentile(row, fluorescence_percentile)
-    percentile_list.append(np.abs(percentile))
+fluorescence_percentile = 10 # set percentile value for calculating baseline fluorescence (f0)
+## TODO: THIS NEEDS TO BE A FUNCTION
+for row in neuropil_subtracted_roi:
+    percentile = np.percentile(row, fluorescence_percentile, keepdims= True)
+    percentile_list.append(percentile)
 baseline_fluorescence = np.array(percentile_list)
 
-print(baseline_fluorescence)
+#print(baseline_fluorescence)
 
-#%%
-dff_roi = []
+# %% CALCULATE DF/F
+## TODO: THIS NEEDS TO BE A FUNCTION
 for row in neuropil_subtracted_roi:
-    dff = ((neuropil_subtracted_roi.T) - (baseline_fluorescence))/ (baseline_fluorescence)
-    dff_roi.append(dff)
-
-print(dff_roi[3])
-
+    dff = ((row) - (baseline_fluorescence))/ (baseline_fluorescence)
 
 # %% PLOTTING DFF 
 import matplotlib.pyplot as plt
 
-roi_number = dff_roi[3]
+plt.plot(dff[71])
+plt.title('Neuronal Ca2+ activity across session')
+plt.xlabel('Frames')
+plt.ylabel('df/f')
+plt.show()
 
-plt.plot(range(len(roi_number)), roi_number)
+# %% CREATE VIS STIM SIMULATED VECTOR
+
+def generate_vis_stim_vector(frame_rate, total_frames, stim_duration, gray_duration): #frames per second, total number of frames in session, grat duration (s), gray duration(s))
+    gray_frames = int(gray_duration * frame_rate) # gray frames in a single trial
+    stim_frames = int(stim_duration * frame_rate) # vis stim frames in a single trial
+    vis_trial_timestamps = np.hstack([ np.zeros(gray_frames),np.ones(stim_frames)]) # fills array with 1 for stim frame, 0 for gray
+    vis_session_timestamps = np.resize (vis_trial_timestamps, total_frames) # 
+    return vis_session_timestamps
+
+simulated_vis_stim = generate_vis_stim_vector(40, 6000, 2, 3)
+# print(simulated_vis_stim[:50])
+
+# %% STIM FRAMES ONLY
+
+
+
+# %% 
+plt.plot(neuropil_subtracted_roi[71][:500])
+plt.plot(simulated_vis_stim[:500])
 plt.show()
 # %%
