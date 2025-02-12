e98da11d703cbe1c1e68429a4a68ff223859b96c
Sindhuja Baskar <153032345+sindhujabaskar@users.noreply.github.com>
2025-02-12 12:58:16 -0500

    fix: neuropil subtraction of only 30% neuropil signal

--- s2p-dff-plotter.py
+++ s2p-dff-plotter.py
@@ -38,7 +38,7 @@ def calculate_baseline(raw_fluorescence, percentile): # this will calculate the 
     return percentile_list
 
 # %% LOAD DATA
-suite2p_data_output = load_suite2p_outputs(r'C:\dev\2p-analysis\suite2p\SB03\tiff\sb03_baseline\suite2p\plane0')
+suite2p_data_output = load_suite2p_outputs(r'C:\dev\2p-analysis\func\SB05\tiff\sb05_high\suite2p\plane0')
     
 # %% FILTER ROIs (CELLS ONLY)
 
@@ -52,7 +52,7 @@ filtered_neuropil = np.array(suite2p_data_output['neuropil_fluorescence'][true_c
 
 # %% NEUROPIL SUBTRACTION
 
-neuropil_subtracted_roi = (filtered_roi - (filtered_neuropil)) #TODO: necessary?
+neuropil_subtracted_roi = (filtered_roi - (0.7*filtered_neuropil)) #TODO: necessary?
 
 #%% PLOT ROIS
 # plt.plot(neuropil_subtracted_roi[2])
@@ -69,6 +69,29 @@ baseline_fluorescence = calculate_baseline(filtered_roi, percentile = 10)
 roi_dff = calculate_dff(filtered_roi, baseline_fluorescence)
 # print(roi_dff)
 
+# %% EXPORT TO CSV
+
+def export_dff_to_csv(dff_data, output_path):
+    # Convert the list of ΔF/F arrays into a DataFrame
+    dff_df = pd.DataFrame(dff_data)
+    
+    # Export to CSV
+    dff_df.to_csv(output_path, index=False)
+    print(f"ΔF/F data successfully exported to {output_path}")
+# %%
+# Specify the output file path
+output_csv_path = r'C:\dev\2p-analysis\func\SB05\analysis\sb05_high_roi_dff_data_robust.csv'
+
+# Export the ΔF/F data
+export_dff_to_csv(roi_dff, output_csv_path)
+
+#%% PLOT ROIS- NEUROPIL SUBTRACTION
+# plt.plot(neuropil_subtracted_roi[2])
+# plt.title('Raw Fluorescence of ROI')
+# plt.xlabel('frames')
+# plt.ylabel('raw fluorescence')
+# plt.show()
+
 # %% PLOTTING DFF 
 # plt.plot(roi_dff[2])
 # plt.title('Neuronal Ca2+ activity across session')
@@ -76,58 +99,39 @@ roi_dff = calculate_dff(filtered_roi, baseline_fluorescence)
 # plt.ylabel('df/f')
 # plt.show()
 
-# %% EXPORT TO CSV
+# %% CALCULATE AVERAGE DFF
+# def calculate_average_roi(dff_data):
+#     """
+#     Calculate the average ΔF/F across all ROIs at each timepoint and export to CSV.
 
-# def export_dff_to_csv(dff_data, output_path):
+#     Parameters:
+#     dff_data (list of numpy arrays): A list where each element is a numpy array of ΔF/F values for an ROI.
+#     output_path (str): The file path where the average data will be saved.
+#     """
 #     # Convert the list of ΔF/F arrays into a DataFrame
 #     dff_df = pd.DataFrame(dff_data).transpose()
     
-#     # Rename columns to indicate ROI numbers
-#     dff_df.columns = [f"ROI_{i}" for i in range(len(dff_data))]
-    
-#     # Export to CSV
-#     dff_df.to_csv(output_path, index=False)
-#     print(f"ΔF/F data successfully exported to {output_path}")
-# # %%
-# # Specify the output file path
-# output_csv_path = r'C:\dev\2p-analysis\suite2p\SB03\tiff\sb03_baseline\suite2p\plane0\roi_dff_data.csv'
+#     # Calculate the mean across all ROIs for each timepoint
+#     average_df = pd.DataFrame(dff_df.mean(axis=1), columns=["Average_ROI"])
 
-# # Export the ΔF/F data
-# export_dff_to_csv(roi_dff, output_csv_path)
+#     return average_df
 
-# %% CALCULATE AVERAGE DFF
-def calculate_average_roi(dff_data):
-    """
-    Calculate the average ΔF/F across all ROIs at each timepoint and export to CSV.
+# # Calculate the average ΔF/F values
+# average_df = calculate_average_roi(roi_dff)
 
-    Parameters:
-    dff_data (list of numpy arrays): A list where each element is a numpy array of ΔF/F values for an ROI.
-    output_path (str): The file path where the average data will be saved.
-    """
-    # Convert the list of ΔF/F arrays into a DataFrame
-    dff_df = pd.DataFrame(dff_data).transpose()
-    
-    # Calculate the mean across all ROIs for each timepoint
-    average_df = pd.DataFrame(dff_df.mean(axis=1), columns=["Average_ROI"])
+# # Export the average values to a CSV
+# average_output_csv_path = r'C:\dev\2p-analysis\suite2p\SB03_analysis\sb03_sal_average_roi_dff_data2.csv'
+# average_df.to_csv(average_output_csv_path, index=False)
+# print(f"Average ROI ΔF/F data successfully exported to {average_output_csv_path}")
 
-    return average_df
-
-# Calculate the average ΔF/F values
-average_df = calculate_average_roi(roi_dff)
-
-# Export the average values to a CSV
-average_output_csv_path = r'C:\dev\2p-analysis\suite2p\SB03_analysis\sb03_baseline_average_roi_dff_data.csv'
-average_df.to_csv(average_output_csv_path, index=False)
-print(f"Average ROI ΔF/F data successfully exported to {average_output_csv_path}")
-
-# Plot the average ΔF/F values
-plt.figure(figsize=(10, 5))
-plt.plot(average_df)
-plt.suptitle('SB03-BASELINE')
-plt.title("Average ΔF/F Across All ROIs")
-plt.xlabel("Time (Frames)")
-plt.ylabel("Average ΔF/F")
-plt.grid()
-plt.show()
+# # Plot the average ΔF/F values
+# plt.figure(figsize=(10, 5))
+# plt.plot(average_df)
+# plt.suptitle('SB03-SAL')
+# plt.title("Average ΔF/F Across All ROIs")
+# plt.xlabel("Time (Frames)")
+# plt.ylabel("Average ΔF/F")
+# plt.grid()
+# plt.show()
 
 # %%
